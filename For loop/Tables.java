4ff75baf8e8a8e95e2a3fdd5dc3d46e0f085c275
Vivek-Vaidya <vaidya764@gmail.com>
2022-08-04 22:56:59 +0530

    For loop folder added

---
+++ For loop/Tables.java
@@ -0,0 +1,10 @@
+public Tables {
+    public static void main(String[] arg) {
+        int a = 5;
+
+        for (int i = 1; i <= 10; i++){
+
+            System.out.println(i*a);
+        }
+    }
+}
