987aa1b572fed375c5ef743dd265573353dccfb8
Vivek-Vaidya <vaidya764@gmail.com>
2022-08-05 15:28:23 +0530

    first intellij program

---
+++ first-intellijidea-program/src/com/rahul/Main.java
@@ -0,0 +1,7 @@
+package com.rahul;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("this is the com.rahul package");
+    }
+}
---
+++ first-intellijidea-program/src/com/vivek/Main.java
@@ -0,0 +1,9 @@
+package com.vivek;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        System.out.println("this is the com.vivek Package");
+    }
+}
